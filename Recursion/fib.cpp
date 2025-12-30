31c693881f0eb8c64e420e0665dec510e361b816
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-12-30 17:19:45 +0530

    Refactor code structure and remove redundant code blocks

---
+++ Recursion/fib.cpp
@@ -0,0 +1,16 @@
+// multiple recursion calls
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Fibonacci number at position " << n << " is " << fib(n) << endl;
+    return 0;
+}
